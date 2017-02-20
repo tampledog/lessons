aa88c0b7030e895ec017a972c7051fe532868f10
admin-ПК\admin <tampledog@gmail.com>
2017-02-20 17:23:04 +0200

    no message

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -2,11 +2,24 @@
 
 
 $(document).ready(function(){
+    $('.tab').click(function () {
+        var ind = $(this).index();
+        console.log(ind);
+        $('.item').removeClass('active');
+        $('.tab').removeClass('active');
+        $('.item').eq(ind).addClass('active');
+        $(this).addClass('active');
 
+    });
 });
 
 $(window).load(function(){
 
+    $('.plus').click(function () {
+        $('li').removeClass('active');
+        $(this).closest('li').addClass('active');
+        $(this).closest('.content').addClass('active');
+    });
 });
 
 $(window).resize(function(){
